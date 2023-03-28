a2b5d37b4905daaf1a41963464bd46b18eb690a5
minttreee3 <mintppp58@gmail.com>
2023-03-28 18:03:51 +0900

    javadb

--- javadb.sql
+++ javadb.sql
@@ -22,3 +22,52 @@ CREATE SEQUENCE user_seq;
 INSERT INTO userTBL VALUES ( user_seq.NEXTVAL , '홍길동', 2010, '서울시 종로구 100로', '010-1234-1234');
 
 COMMIT; -- DML작업시 한번에 시행되어야 하는 단위, DML작업한후 COMMIT(DB최종반영)해야함
+
+
+
+
+-- [자바 SHOP 연동]
+-- 모든 컬럼 not null
+-- paytype : pay_no(숫자-1 pk), info(문자-card,cash)  
+-- paytype_seq 생성
+CREATE TABLE paytype (
+pay_no number(1) primary key,
+info VARCHAR2(4) not null
+);
+
+CREATE SEQUENCE paytype_seq;
+
+insert into paytype values (paytype_seq.NEXTVAL, 'card');
+insert into paytype values (paytype_seq.NEXTVAL, 'cash');
+
+
+-- suser : user_id(숫자-4 pk), name(문자-한글), pay_no(숫자-1 : paytype 테이블에 있는 pay_no 참조해서 사용)
+create table suser (
+user_id number(4) primary key,
+name VARCHAR2(15) not null,
+pay_no number(1) not null references paytype(pay_no)
+);
+
+-- product
+-- product_id(숫자-8 pk), pname(문자), price(숫자), content(문자)
+create table product (
+product_id number(8) primary key,
+pname VARCHAR2(30) not null,
+price number(10) not null,
+content VARCHAR2(30) not null
+);
+
+create sequence product_seq;
+
+-- sorder
+-- order_id(숫자-8 pk), user_id(user테이블참조), product_id(product테이블참조)
+-- order_seq
+create table sorder (
+order_id number(8) primary key,
+user_id number(4) not null references suser(user_id),
+product_id number(8) not null references product(product_id)
+);
+
+create sequence order_seq;
+
+
